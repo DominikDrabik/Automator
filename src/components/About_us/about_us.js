75ca91f4e75b111249e1692ac2d10250930ca975
Dominik Drabik <drabikdominik@op.pl>
2023-11-12 18:14:01 +0100

    Initial commit website

---
+++ src/components/About_us/about_us.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './about_us.css';
+
+const About_us = () => {
+  return (
+    <section id="about_us">
+        <h1>What problems does it solve</h1>
+        <div className="tiles">
+            
+        </div>
+    </section>
+  )
+}
+
+export default About_us
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,63 @@
+import React, {useState, useEffect} from 'react';
+import './footer.css';
+import { Link } from 'react-router-dom';
+
+
+
+
+
+const Footer = () => {
+
+
+  return (
+    <section id="Footer">
+      <footer className="footer">
+  <div className="container">
+    <div className="row">
+      <div className="footer-col">
+        <h4>company</h4>
+        <ul>
+          <li>
+            <Link to="/privacy-policy">Privacy Policy</Link>
+          </li>
+          <li>
+            <Link to="/">&copy; 2023 Dominik Drabik</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="footer-col">
+        <h4>Navigation</h4>
+        <ul>
+          <li>
+            <Link to="/">Download</Link>
+          </li>
+          <li>
+            <Link to="/how-to-use">How to Use</Link>
+          </li>
+          <li>
+            <Link to="/example">Example</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-col">
+        <h4>follow us</h4>
+        <div className="social-links">
+          <a href="mailto:drabikdominik@op.pl">
+            <i className="fas fa-envelope"></i>
+          </a>
+          
+          <a href="https://www.linkedin.com/in/dominik-drabik/">            
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+    </section>
+  )
+}
+
+export default Footer;
