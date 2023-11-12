962cb748d329b98de5ccb6c4453f53f7a0bbd87f
Dominik Drabik <drabikdominik@op.pl>
2023-11-12 18:25:24 +0100

    Updates

---
+++ precache-manifest.50d5df02fa62f0ab034a3b7b5315cfdd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c76bcce46a3253e293a1ff806d4d3f3",
+    "url": "/Automator/index.html"
+  },
+  {
+    "revision": "0b535dda1c990189b398",
+    "url": "/Automator/static/css/main.d657e373.chunk.css"
+  },
+  {
+    "revision": "59169d982511921f2c4c",
+    "url": "/Automator/static/js/2.ea056b36.chunk.js"
+  },
+  {
+    "revision": "0b535dda1c990189b398",
+    "url": "/Automator/static/js/main.6a1fa1d6.chunk.js"
+  },
+  {
+    "revision": "e9a90b0ec0b91a7d4395",
+    "url": "/Automator/static/js/runtime~main.fc8d51e6.js"
+  },
+  {
+    "revision": "a3de60b61a1f4589d54b0ea0f1571344",
+    "url": "/Automator/static/media/automation.a3de60b6.png"
+  },
+  {
+    "revision": "d4b737bb5313ea1044256c7cff5bcb43",
+    "url": "/Automator/static/media/automation_new_presenstation.d4b737bb.mp4"
+  },
+  {
+    "revision": "6dcdc0a086a164cbdbd8384f7f20365e",
+    "url": "/Automator/static/media/automator_logo_v1.6dcdc0a0.png"
+  },
+  {
+    "revision": "d323e68917126e82e0e2acc633d942dc",
+    "url": "/Automator/static/media/open_menu5.d323e689.svg"
+  }
+]);
